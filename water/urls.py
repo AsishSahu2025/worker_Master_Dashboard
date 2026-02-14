fb486b3fa522784c258bd944fa635a1cd311b3bf
bariflo <web8.bariflo@outlook.com>
2026-02-14 12:56:38 +0530

    admin css

--- water/urls.py
+++ water/urls.py
@@ -107,5 +107,3 @@ urlpatterns = [
  
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
